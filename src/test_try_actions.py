7dce1d2bf1ef1d3cecf7952438911063d238f70d
athrva <atharva251102@gmail.com>
2025-04-21 18:24:17 +0530

    adding more tests

--- src/test_try_actions.py
+++ src/test_try_actions.py
@@ -5,8 +5,8 @@ from try_actions import sum
 def test_sum():
     assert sum(3, 5) == 8
 
-# def test_sum():
-#     assert sum("hello", "world") == "helloworld"
+def test_sum():
+    assert sum("hello", "world") == "helloworld"
 
-# def test_sum():
-#     assert sum([1, 2], [3, 4]) == [1, 2, 3, 4]
+def test_sum():
+    assert sum([1, 2], [3, 4]) == [1, 2, 3, 4]
