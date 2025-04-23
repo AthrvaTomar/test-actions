e008a49678d2a6fbec063cfee5c4a2275a54598b
athrva <atharva251102@gmail.com>
2025-04-23 14:40:17 +0530

    changes in test file

--- src/test_try_actions.py
+++ src/test_try_actions.py
@@ -6,27 +6,17 @@ from try_actions import sub
 
 def test_sum():
     assert sum(3, 5) == 9
-
-def test_sum():
     assert sum("hello", "world") == "helloworld"
-
-def test_sum():
     assert sum([1, 2], [3, 4]) == [1, 2, 3, 4]
 
 def test_mul():
     assert mul(3, 5) == 15
-
-def test_mul():
     assert mul("hello", 3) == "hellohellohello"
-
-def test_mul():
     assert mul([1, 2], 3) == [1, 2, 1, 2, 1, 2]
+
 
 def test_sub():
     assert sub(10, 5) == 5
+    # assert sub("hello", "world") == "helloworld"
+    assert sub([1, 2, 3], [1, 1, 1]) == [0, 1, 2]
 
-def test_sub():
-    assert sub(1, 2) == -1
-
-def test_sub():     
-    assert sub([1, 2], [3, 1]) == [-2, 1]
